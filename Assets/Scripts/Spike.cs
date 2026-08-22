ae027e5061fd3cf375725656a8e7aca9352da2b1
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-08-22 17:42:33 +0400

    Added support for upper spikes

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -7,17 +7,20 @@ public class Spike : MonoBehaviour
     [SerializeField]
     private int speed = 50;
 
+    [SerializeField]
+    private bool upperSpike = false;
+
     void Start() { }
 
     // Update is called once per frame
     void Update() { }
 
-    void Fold()
+    public void Fold()
     {
         StartCoroutine(FoldCoroutine());
     }
 
-    void Unfold()
+    public void Unfold()
     {
         StartCoroutine(UnfoldCoroutine());
     }
@@ -29,11 +32,22 @@ public class Spike : MonoBehaviour
         {
             elapsed += Time.deltaTime;
 
-            transform.position = Vector2.MoveTowards(
-                transform.position,
-                new Vector2(transform.position.x, -4),
-                speed * Time.deltaTime
-            );
+            if (upperSpike)
+            {
+                transform.position = Vector2.MoveTowards(
+                    transform.position,
+                    new Vector2(transform.position.x, 4),
+                    speed * Time.deltaTime
+                );
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(
+                    transform.position,
+                    new Vector2(transform.position.x, -4),
+                    speed * Time.deltaTime
+                );
+            }
             yield return null;
         }
     }
@@ -45,11 +59,22 @@ public class Spike : MonoBehaviour
         {
             elapsed += Time.deltaTime;
 
-            transform.position = Vector2.MoveTowards(
-                transform.position,
-                new Vector2(transform.position.x, -9),
-                speed * Time.deltaTime
-            );
+            if (upperSpike)
+            {
+                transform.position = Vector2.MoveTowards(
+                    transform.position,
+                    new Vector2(transform.position.x, 9),
+                    speed * Time.deltaTime
+                );
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(
+                    transform.position,
+                    new Vector2(transform.position.x, -9),
+                    speed * Time.deltaTime
+                );
+            }
             yield return null;
         }
     }
