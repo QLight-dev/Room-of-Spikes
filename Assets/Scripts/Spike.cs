97a985d3f28d500c2bff8252931eb4b3bdb225c7
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-08-22 17:11:05 +0400

    Gave spikes the ability to fold and unfold

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,16 +1,56 @@
+using System.Collections;
+using System.Timers;
 using UnityEngine;
 
 public class Spike : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private int speed = 50;
+
+    void Start() { }
 
     // Update is called once per frame
-    void Update()
+    void Update() { }
+
+    void Fold()
     {
-        
+        StartCoroutine(FoldCoroutine());
+    }
+
+    void Unfold()
+    {
+        StartCoroutine(UnfoldCoroutine());
+    }
+
+    IEnumerator FoldCoroutine()
+    {
+        float elapsed = 0f;
+        while (elapsed < 0.5f)
+        {
+            elapsed += Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                new Vector2(transform.position.x, -4),
+                speed * Time.deltaTime
+            );
+            yield return null;
+        }
+    }
+
+    IEnumerator UnfoldCoroutine()
+    {
+        float elapsed = 0f;
+        while (elapsed < 0.5f)
+        {
+            elapsed += Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(
+                transform.position,
+                new Vector2(transform.position.x, -9),
+                speed * Time.deltaTime
+            );
+            yield return null;
+        }
     }
 }
